39e1e442f41542ca82b9ad5be372c88cc36ab91c
andrea madotto <samurai@andreas-MacBook-Pro.local>
2017-01-30 09:09:18 +0100

    minor

--- testPHI.sh
+++ testPHI.sh
@@ -1,36 +1,36 @@
-#!/bin/bash
+#!/bash
 echo ''
 echo 'thread number 1'
-./bin/jacobiParPHI 1000 10 0.01 1
-./bin/jacobiforPHI 1000 10 0.01 1
+./jacobiParPHI 1000 10 0.01 1
+./jacobiForPHI 1000 10 0.01 1
 echo ''
 echo 'thread number 2'
-./bin/jacobiParPHI 1000 10 0.01 2
-./bin/jacobiforPHI 1000 10 0.01 2
+./jacobiParPHI 1000 10 0.01 2
+./jacobiForPHI 1000 10 0.01 2
 echo ''
 echo 'thread number 3'
-./bin/jacobiParPHI 1000 10 0.01 3
-./bin/jacobiforPHI 1000 10 0.01 3
+./jacobiParPHI 1000 10 0.01 3
+./jacobiForPHI 1000 10 0.01 3
 echo ''
 echo 'thread number 4'
-./bin/jacobiParPHI 1000 10 0.01 4
-./bin/jacobiforPHI 1000 10 0.01 4
+./jacobiParPHI 1000 10 0.01 4
+./jacobiForPHI 1000 10 0.01 4
 
 echo ''
 echo 'new test'
 echo ''
 echo 'thread number 1'
-./bin/jacobiParPHI 1000 1000 0.01 1
-./bin/jacobiForPHI 1000 1000 0.01 1
+./jacobiParPHI 1000 1000 0.01 1
+./jacobiForPHI 1000 1000 0.01 1
 echo ''
 echo 'thread number 2'
-./bin/jacobiParPHI 1000 1000 0.01 2
-./bin/jacobiForPHI 1000 1000 0.01 2
+./jacobiParPHI 1000 1000 0.01 2
+./jacobiForPHI 1000 1000 0.01 2
 echo ''
 echo 'thread number 3'
-./bin/jacobiParPHI 1000 1000 0.01 3
-./bin/jacobiForPHI 1000 1000 0.01 3
+./jacobiParPHI 1000 1000 0.01 3
+./jacobiForPHI 1000 1000 0.01 3
 echo ''
 echo 'thread number 4'
-./bin/jacobiParPHI 1000 1000 0.01 4
-./bin/jacobiForPHI 1000 1000 0.01 4
+./jacobiParPHI 1000 1000 0.01 4
+./jacobiForPHI 1000 1000 0.01 4
--- testXEON.sh
+++ testXEON.sh
@@ -3,19 +3,19 @@
 echo ''
 echo 'thread number 1'
 ./bin/jacobiParXEON 1000 10 0.01 1
-./bin/jacobiforXEON 1000 10 0.01 1
+./bin/jacobiForXEON 1000 10 0.01 1
 echo ''
 echo 'thread number 2'
 ./bin/jacobiParXEON 1000 10 0.01 2
-./bin/jacobiforXEON 1000 10 0.01 2
+./bin/jacobiForXEON 1000 10 0.01 2
 echo ''
 echo 'thread number 3'
 ./bin/jacobiParXEON 1000 10 0.01 3
-./bin/jacobiforXEON 1000 10 0.01 3
+./bin/jacobiForXEON 1000 10 0.01 3
 echo ''
 echo 'thread number 4'
 ./bin/jacobiParXEON 1000 10 0.01 4
-./bin/jacobiforXEON 1000 10 0.01 4
+./bin/jacobiForXEON 1000 10 0.01 4
 
 echo ''
 echo 'new test'
