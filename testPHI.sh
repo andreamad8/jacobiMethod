5ddca17fb9f5f4d499e652a058cb7c4e0f827009
andrea madotto <samurai@host-131-114-229-223.m.unipi.it>
2017-02-01 14:05:13 +0100

    minor

--- testPHI.sh
+++ testPHI.sh
@@ -6,7 +6,6 @@
 ./bin/jacobiserPHI 7000  50 0.000001 > ris/serPHI_07000_500_0001
 ./bin/jacobiserPHI 10000 50 0.000001 > ris/serPHI_010000_500_0001
 ./bin/jacobiserPHI 15000 50 0.000001 > ris/serPHI_015000_500_0001
-
 ./bin/jacobiParPHI 100   50 0.000001 90 5 > ris/ParPHI_0100_500_0001
 ./bin/jacobiParPHI 500   50 0.000001 90 5 > ris/ParPHI_0500_500_0001
 ./bin/jacobiParPHI 1000  50 0.000001 90 5 > ris/ParPHI_01000_500_0001
@@ -14,7 +13,6 @@
 ./bin/jacobiParPHI 7000  50 0.000001 90 5 > ris/ParPHI_07000_500_0001
 ./bin/jacobiParPHI 10000 50 0.000001 90 5 > ris/ParPHI_010000_500_0001
 ./bin/jacobiParPHI 15000 50 0.000001 90 5 > ris/ParPHI_015000_500_0001
-
 ./bin/jacobiForPHI 100   50 0.000001 90 5 > ris/ForPHI_0100_500_0001
 ./bin/jacobiForPHI 500   50 0.000001 90 5 > ris/ForPHI_0500_500_0001
 ./bin/jacobiForPHI 1000  50 0.000001 90 5 > ris/ForPHI_01000_500_0001
--- testXEON.sh
+++ testXEON.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-./bin/jacobiserXEON 100   500 0.0001 > ris/serXEON_0100_500_0001
-./bin/jacobiserXEON 500   500 0.0001 > ris/serXEON_0500_500_0001
-./bin/jacobiserXEON 1000  500 0.0001 > ris/serXEON_01000_500_0001
-./bin/jacobiserXEON 5000  500 0.0001 > ris/serXEON_05000_500_0001
-./bin/jacobiserXEON 7000  500 0.0001 > ris/serXEON_05000_500_0001
-./bin/jacobiserXEON 10000 500 0.0001 > ris/serXEON_010000_500_0001
-./bin/jacobiserXEON 15000 500 0.0001 > ris/serXEON_05000_500_0001
-
-./bin/jacobiParXEON 100   500 0.0001 17 1 > ris/ParXEON_0100_500_0001
-./bin/jacobiParXEON 500   500 0.0001 17 1 > ris/ParXEON_0500_500_0001
-./bin/jacobiParXEON 1000  500 0.0001 17 1 > ris/ParXEON_01000_500_0001
-./bin/jacobiParXEON 5000  500 0.0001 17 1 > ris/ParXEON_05000_500_0001
-./bin/jacobiParXEON 7000  500 0.0001 17 1 > ris/ParXEON_07000_500_0001
-./bin/jacobiParXEON 10000 500 0.0001 17 1 > ris/ParXEON_010000_500_0001
-./bin/jacobiParXEON 15000 500 0.0001 17 1 > ris/ParXEON_015000_500_0001
-
-./bin/jacobiForXEON 100   500 0.0001 17 1 > ris/ParXEON_0100_500_0001
-./bin/jacobiForXEON 500   500 0.0001 17 1 > ris/ParXEON_0500_500_0001
-./bin/jacobiForXEON 1000  500 0.0001 17 1 > ris/ParXEON_01000_500_0001
-./bin/jacobiForXEON 5000  500 0.0001 17 1 > ris/ParXEON_05000_500_0001
-./bin/jacobiForXEON 7000  500 0.0001 17 1 > ris/ParXEON_07000_500_0001
-./bin/jacobiForXEON 10000 500 0.0001 17 1 > ris/ParXEON_010000_500_0001
-./bin/jacobiForXEON 15000 500 0.0001 17 1 > ris/ParXEON_015000_500_0001
+./bin/jacobiserXEON 100   50 0.000001 > ris/serXEON_0100
+./bin/jacobiserXEON 500   50 0.000001 > ris/serXEON_0500
+./bin/jacobiserXEON 1000  50 0.000001 > ris/serXEON_01000
+./bin/jacobiserXEON 5000  50 0.000001 > ris/serXEON_05000
+./bin/jacobiserXEON 7000  50 0.000001 > ris/serXEON_05000
+./bin/jacobiserXEON 10000 50 0.000001 > ris/serXEON_010000
+./bin/jacobiserXEON 15000 50 0.000001 > ris/serXEON_015000
+./bin/jacobiParXEON 100   50 0.000001 17 1 > ris/ParXEON_0100
+./bin/jacobiParXEON 500   50 0.000001 17 1 > ris/ParXEON_0500
+./bin/jacobiParXEON 1000  50 0.000001 17 1 > ris/ParXEON_01000
+./bin/jacobiParXEON 5000  50 0.000001 17 1 > ris/ParXEON_05000
+./bin/jacobiParXEON 7000  50 0.000001 17 1 > ris/ParXEON_07000
+./bin/jacobiParXEON 10000 50 0.000001 17 1 > ris/ParXEON_010000
+./bin/jacobiParXEON 15000 50 0.000001 17 1 > ris/ParXEON_015000
+./bin/jacobiForXEON 100   50 0.000001 17 1 > ris/ForXEON_0100
+./bin/jacobiForXEON 500   50 0.000001 17 1 > ris/ForXEON_0500
+./bin/jacobiForXEON 1000  50 0.000001 17 1 > ris/ForXEON_01000
+./bin/jacobiForXEON 5000  50 0.000001 17 1 > ris/ForXEON_05000
+./bin/jacobiForXEON 7000  50 0.000001 17 1 > ris/ForXEON_07000
+./bin/jacobiForXEON 10000 50 0.000001 17 1 > ris/ForXEON_010000
+./bin/jacobiForXEON 15000 50 0.000001 17 1 > ris/ForXEON_015000
