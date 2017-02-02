f25f713692f7a2fe4d355d05c55b6e203430dbd8
andrea madotto <samurai@andreas-MacBook-Pro.local>
2017-02-02 23:32:42 +0100

    minor lib

--- src/jacobiser.cpp
+++ src/jacobiser.cpp
@@ -103,8 +103,6 @@ int main(int argc, char const *argv[]) {
     for (size_t k = 0; k <= maxiter; k++) {
       for (int i = 0; i < N; i++) {
         sum = -A[i][i] * x[i];
-#pragma vector aligned
-#pragma ivdep
         for (int j = 0; j < N; j++) {
           sum += A[i][j] * x[j];
         }
