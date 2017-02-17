77d9d8ebce3acd9f66c330baf2cfb63a0665d608
andrea madotto <samurai@andreas-MacBook-Pro.local>
2017-02-17 19:35:21 +0100

    minor lib

--- testPHI.sh
+++ testPHI.sh
@@ -1,12 +1,12 @@
 #!/bash
 #./bin/jacobiserPHI 100   50 0.000001 > ris/serPHI_0100_500_0001
 #./bin/jacobiserPHI 500   50 0.000001 > ris/serPHI_0500_500_0001
-./bin/jacobiserPHI 1000  50 0.000001 > ris/serPHI_01000_500_0001
-./bin/jacobiserPHI 5000  50 0.000001 > ris/serPHI_05000_500_0001
+#./bin/jacobiserPHI 1000  50 0.000001 > ris/serPHI_01000_500_0001
+#./bin/jacobiserPHI 5000  50 0.000001 > ris/serPHI_05000_500_0001
 #./bin/jacobiserPHI 7000  50 0.000001 > ris/serPHI_07000_500_0001
-./bin/jacobiserPHI 10000 50 0.000001 > ris/serPHI_010000_500_0001
-./bin/jacobiserPHI 15000 50 0.000001 > ris/serPHI_015000_500_0001
-./bin/jacobiserPHI 30000 50 0.000001 > ris/serPHI_030000_500_0001
+#./bin/jacobiserPHI 10000 50 0.000001 > ris/serPHI_010000_500_0001
+#./bin/jacobiserPHI 15000 50 0.000001 > ris/serPHI_015000_500_0001
+#./bin/jacobiserPHI 30000 50 0.000001 > ris/serPHI_030000_500_0001
 #./bin/jacobiParPHI 100   50 0.000001 255 5 > ris/ParPHI_0100_500_0001
 #./bin/jacobiParPHI 500   50 0.000001 255 5 > ris/ParPHI_0500_500_0001
 ./bin/jacobiParPHI 1000  50 0.000001 255 5 > ris/ParPHI_01000_500_0001
@@ -17,12 +17,12 @@
 ./bin/jacobiParPHI 30000 50 0.000001 250 10 > ris/ParPHI_030000_500_0001
 #./bin/jacobiForPHI 100   50 0.000001 255 5 > ris/ForPHI_0100_500_0001
 #./bin/jacobiForPHI 500   50 0.000001 255 5 > ris/ForPHI_0500_500_0001
-./bin/jacobiForPHI 1000  50 0.000001 255 5 > ris/ForPHI_01000_500_0001
-./bin/jacobiForPHI 5000  50 0.000001 255 5 > ris/ForPHI_05000_500_0001
+#./bin/jacobiForPHI 1000  50 0.000001 255 5 > ris/ForPHI_01000_500_0001
+#./bin/jacobiForPHI 5000  50 0.000001 255 5 > ris/ForPHI_05000_500_0001
 #./bin/jacobiForPHI 7000  50 0.000001 255 5 > ris/ForPHI_07000_500_0001
-./bin/jacobiForPHI 10000 50 0.000001 255 5 > ris/ForPHI_010000_500_0001
-./bin/jacobiForPHI 15000 50 0.000001 255 5 > ris/ForPHI_015000_500_0001
-./bin/jacobiForPHI 30000 50 0.000001 250 10 > ris/ForPHI_030000_500_0001
+#./bin/jacobiForPHI 10000 50 0.000001 255 5 > ris/ForPHI_010000_500_0001
+#./bin/jacobiForPHI 15000 50 0.000001 255 5 > ris/ForPHI_015000_500_0001
+#./bin/jacobiForPHI 30000 50 0.000001 250 10 > ris/ForPHI_030000_500_0001
 
 
 #./bin/jacobiserPHI 30000 50 0.000001 > ris/serPHI_030000_500_0001
